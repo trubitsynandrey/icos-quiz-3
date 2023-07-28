c6d09cf1f49acec7bea1e61a290cb97359d14844
DanilovG103 <danilov2706@gmail.com>
2023-07-28 18:51:01 +0400

    feat: connect metrika

--- src/utils/send-goal.ts
+++ src/utils/send-goal.ts
@@ -1,5 +1,5 @@
 export const sendGoal = (name: string, params?: unknown) => {
   setTimeout(() => {
-    window.ym(94222395, 'reachGoal', name, params)
+    window.ym(94470468, 'reachGoal', name, params)
   }, 2000)
 }
